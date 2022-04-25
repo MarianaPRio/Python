c079bd26f8ef7319dce864091f69c3e16ce87377
MarianaPRio <marianapires00@gmail.com>
2022-04-24 22:16:50 -0300

    Inicio
    Signed-off-by: MarianaPRio <marianapires00@gmail.com>

---
+++ adivinhacao.py
@@ -0,0 +1,31 @@
+from unittest import TextTestResult
+
+
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1 
+
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu numero: ")
+    print("Voce digitou ", chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Voce acertou!")
+    else:
+        if(maior):
+            print("Seu chute foi maior que o numero secreto!")
+        elif(menor):
+            print("Seu chute foi menor que o numero secreto!")     
+    rodada = rodada + 1
+    print("\n")
+print("Fim de jogo.")
