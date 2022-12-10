9c8f149506347b00b840ae4ebcbbd8e6fcc8a175
MarianaPRio <marianapires00@gmail.com>
2022-12-10 09:52:18 -0300

    recomecando
    Signed-off-by: MarianaPRio <marianapires00@gmail.com>

--- forca.py
+++
@@ -1,9 +0,0 @@
-def jogo_forca():
-    print("*********************************")
-    print("***Bem vindo ao jogo de Forca!**")
-    print("*********************************")
-
-    print("Fim de jogo.")
-
-if(__name__ == "__main__"):
-    jogo_forca()
